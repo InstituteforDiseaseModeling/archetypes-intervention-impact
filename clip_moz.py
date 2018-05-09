02acdc0b38ca532dfd80b9d8eb4611517c506a7c
Amelia Bertozzi-Villa <bertozzivill@gmail.com>
2018-05-09 01:52:45 -0700

    extract map 'true' prevalence

--- clip_moz.py
+++ clip_moz.py
@@ -7,12 +7,15 @@ import shapely
 from spatial import run_all_clipping, plot_all_shapes, make_shapefile, plot_shape, extract_latlongs
 
 rasters_to_clip = {
+    "prevalence": {"input_path": "Z:/cubes/Pf_results/MODEL_43/output_rasters/PfPR/Outputs/",
+                  "input_pattern": "MODEL43\.2015.*\.PR\.tif$",
+                   "unit": "sample"},
     # "synoptic_temp": {"input_path": "Z:/mastergrids/Other_Global_Covariates/WorldClim_Temperature/1k/",
     #                   "input_pattern": "WorldClim_Max_Temp_[0-9]{2}_1k\.tif$",
     #                   "unit": "month"},
-    "facebook_pop": {"input_path": "C:/Users/abertozzivilla/Dropbox (IDM)/Malaria Team Folder/data/Mozambique/facebook_population_rasters/",
-                     "input_pattern": "hrsl_moz_pop\.tif$",
-                     "unit": "none"}
+    # "facebook_pop": {"input_path": "C:/Users/abertozzivilla/Dropbox (IDM)/Malaria Team Folder/data/Mozambique/facebook_population_rasters/",
+    #                  "input_pattern": "hrsl_moz_pop\.tif$",
+    #                  "unit": "none"}
     # "baseline_incidence": {"input_path": "Z:/cubes/Pf_results/MODEL_43/output_rasters/incidence/ALL/",
     #                             "input_pattern": "MODEL43\.2015\.[0-9]{1}\.inc\.rate\.PR\.ALL\.tif$"}
 }
@@ -23,7 +26,7 @@ shapefile_name = "bbox"
 data_path = "C:/Users/abertozzivilla/Dropbox (IDM)/Malaria Team Folder/data/Mozambique/Magude"
 grid_path = os.path.join(main_path, "gridded_simulation_input/grid_lookup_friction.csv")
 concave_alpha = 10
-extract_points = False
+extract_points = True
 
 print("loading and cleaning point data")
 grid_data = pd.read_csv(grid_path)
@@ -74,4 +77,5 @@ for input_name, input_vals in rasters_to_clip.items():
             raster_list.append(this_df)
 
         raster_df = pd.concat(raster_list)
+        raster_df.rename(columns={"lat": "mid_y", "lng":"mid_x"}, inplace=True)
         raster_df.to_csv(os.path.join(raster_dir, "grid_vals.csv"), index=False)
