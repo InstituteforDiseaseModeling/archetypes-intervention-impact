8d28e734365e9252909a6538a0f3f691ead7dc9d
Amelia Bertozzi-Villa <bertozzivill@gmail.com>
2018-07-04 11:45:28 -0700

    add functionality for serialization pulls, reduce number of input paramters needed

--- intervention_impact/intervention_simulation.py
+++ intervention_impact/intervention_simulation.py
@@ -1,5 +1,6 @@
 import os
 import pandas as pd
+import numpy as np
 
 from simtools.SetupParser import SetupParser
 
@@ -11,20 +12,28 @@ from simtools.Utilities.COMPSUtilities import COMPS_login
 from malaria.reports.MalariaReport import add_summary_report
 from sweep_functions import add_annual_itns, add_irs_group, add_healthseeking_by_coverage, site_simulation_setup
 
+# variables
+run_type = "burnin"  # set to "burnin" or "intervention"
+burnin_id = "b5f4c942-b67f-e811-a2c0-c4346bcb7275"
+
+# Serialization
+if run_type == "burnin":
+    years = 50
+    exp_name = "Intervention_Impact_Burnins"
+    serialize = True
+    pull_from_serialization = False
+elif run_type == "intervention":
+    years = 3
+    exp_name = "Intervention_Impact_Interventions"
+    serialize = False
+    pull_from_serialization = True
+else:
+    raise ValueError("Unknown run type " + run_type)
+
 # setup
 location = "HPC"
 SetupParser.default_block = location
-years = 3
-
 sites = pd.read_csv("site_details.csv")
-
-# Serialization
-serialize = True  # If true, save serialized files
-pull_from_serialization = False  # requires experiment id
-burnin_id = "put_id_here"
-
-# todo: just different names for burnin vs intervention
-exp_name = "test_multisite_sweep"
 
 cb = DTKConfigBuilder.from_defaults("MALARIA_SIM",
                                     Simulation_Duration=int(365*years),
@@ -66,21 +75,21 @@ if pull_from_serialization:
     df = pd.DataFrame([x.tags for x in expt.simulations])
     df["outpath"] = pd.Series([sim.get_path() for sim in expt.simulations])
 
-    # todo: function for site-specific simulation setup given serialization df
     builder = ModBuilder.from_list([[
         ModFn(DTKConfigBuilder.set_param, "Serialized_Population_Path", os.path.join(df["outpath"][x], "output")),
         ModFn(DTKConfigBuilder.set_param, "Serialized_Population_Filenames",
               [name for name in os.listdir(os.path.join(df["outpath"][x], "output")) if "state" in name]),
         ModFn(DTKConfigBuilder.set_param, "Run_Number", df["Run_Number"][x]),
         ModFn(DTKConfigBuilder.set_param, "x_Temporary_Larval_Habitat", df["x_Temporary_Larval_Habitat"][x]),
-        ModFn(add_annual_itns, year_count=years, n_rounds=1, coverage=y/100, discard_halflife=180),
-        ModFn(add_healthseeking_by_coverage, coverage=z/100),
-        ModFn(add_irs_group, coverage=q/100, decay=180, start_days=[365*start for start in range(years)])
+        ModFn(add_annual_itns, year_count=years, n_rounds=1, coverage=itn_cov/100, discard_halflife=180),
+        ModFn(add_healthseeking_by_coverage, coverage=irs_cov/100),
+        ModFn(add_irs_group, coverage=act_cov/100, decay=180, start_days=[365*start for start in range(years)]),
+        ModFn(site_simulation_setup, site_name=df["Site_Name"][x])
                                      ]
         for x in df.index
-        for y in [0, 80]
-        for z in [0, 80]
-        for q in [0, 80]
+        for itn_cov in [0, 80]
+        for irs_cov in [0, 80]
+        for act_cov in [0, 80]
 
     ])
 else:
@@ -89,9 +98,8 @@ else:
         ModFn(DTKConfigBuilder.set_param, "Run_Number", y),
         ModFn(site_simulation_setup, site_name=site_name)
         ]
-        # for x in np.concatenate((np.arange(0, 2.25, 0.05), np.arange(2.25, 4.25, 0.25)))
-        for x in [1]
-        for y in range(1)
+        for x in np.concatenate((np.arange(0, 2.25, 0.05), np.arange(2.25, 4.25, 0.25)))
+        for y in range(5)
         for site_name in sites["name"]
     ])
 
--- intervention_impact/sweep_functions.py
+++ intervention_impact/sweep_functions.py
@@ -1,4 +1,3 @@
-
 import pandas as pd
 import os
 import json
@@ -9,21 +8,22 @@ from dtk.interventions.health_seeking import add_health_seeking
 from dtk.interventions.irs import add_IRS
 from dtk.interventions.itn_age_season import add_ITN_age_season
 
-def site_simulation_setup(cb, site_name, pull_serialized=True, max_larval_capacity=4e8):
+
+def site_simulation_setup(cb, site_name, max_larval_capacity=4e8):
     sites = pd.read_csv("site_details.csv")
-    this_site = sites.query('name==@site_name')
+    this_site = sites.query("name==@site_name")
 
     # directories
     cb.update_params({
                     "Demographics_Filenames": ["demog/demog_{site}.json".format(site=site_name)],
                     "Air_Temperature_Filename": os.path.join("climate", site_name,
-                                                           "air_temperature_daily.bin"),
-                    "Land_Temperature_Filename": os.path.join('climate', site_name,
-                                                           "air_temperature_daily.bin"),
+                                                             "air_temperature_daily.bin"),
+                    "Land_Temperature_Filename": os.path.join("climate", site_name,
+                                                              "air_temperature_daily.bin"),
                     "Rainfall_Filename": os.path.join("climate", site_name,
-                                                           "rainfall_daily.bin"),
+                                                      "rainfall_daily.bin"),
                     "Relative_Humidity_Filename": os.path.join("climate", site_name,
-                                                           "relative_humidity_daily.bin")
+                                                               "relative_humidity_daily.bin")
                     }
     )
 
@@ -38,18 +38,19 @@ def site_simulation_setup(cb, site_name, pull_serialized=True, max_larval_capaci
     set_params_by_species(cb.params, [name for name in vectors.keys()])
 
     for species_name, species_prop in vectors.items():
-        set_species_param(cb, species_name, 'Adult_Life_Expectancy', 20)
+        set_species_param(cb, species_name, "Adult_Life_Expectancy", 20)
 
         species_modifications = species_details[species_name]
 
         for param, val in species_modifications.items():
-            if param=='habitat_split':
+            if param == "habitat_split":
                 new_vals = {hab: hab_prop * species_prop * max_larval_capacity for hab, hab_prop in val.items()}
-                set_species_param(cb, species_name, 'Larval_Habitat_Types', new_vals)
+                set_species_param(cb, species_name, "Larval_Habitat_Types", new_vals)
             else:
                 set_species_param(cb, species_name, param, val)
 
-    return {"Site_Name": site_name, "Vectors": vectors} # todo: can comps accept a dict as a key
+    return {"Site_Name": site_name, "Vectors": vectors}
+
 
 # itns
 def add_annual_itns(cb, year_count=1, n_rounds=1, coverage=0.8, discard_halflife=270, start_day=0):
@@ -58,15 +59,16 @@ def add_annual_itns(cb, year_count=1, n_rounds=1, coverage=0.8, discard_halflife
     per_round_coverage = 1 - (1 - coverage) ** (1 / n_rounds)
 
     for year in range(year_count):
-        for round in range(n_rounds):
+        for this_round in range(n_rounds):
 
             add_ITN_age_season(cb,
-                               coverage_all = per_round_coverage,
-                               discard = {'halflife': discard_halflife},
-                               start=( 365 *year ) +( 30 *round ) +start_day)
+                               coverage_all=per_round_coverage,
+                               discard={"halflife": discard_halflife},
+                               start=(365 * year) + (30 * this_round) + start_day)
 
-    return {'ITN_Coverage': coverage, 'ITN_Halflife': discard_halflife, 'ITN_Per_Round_Coverage': per_round_coverage,
-            'ITN_start': start_day, 'ITN_Rounds': n_rounds}
+    return {"ITN_Coverage": coverage, "ITN_Halflife": discard_halflife, "ITN_Per_Round_Coverage": per_round_coverage,
+            "ITN_start": start_day, "ITN_Rounds": n_rounds}
+
 
 # irs
 def add_irs_group(cb, coverage=1.0, start_days=[0], decay=270):
@@ -84,22 +86,26 @@ def add_irs_group(cb, coverage=1.0, start_days=[0], decay=270):
         }}
 
     for start in start_days:
-        add_IRS(cb, start, [{'min': 0, 'max': 200, 'coverage': coverage}],
+        add_IRS(cb, start, [{"min": 0, "max": 200, "coverage": coverage}],
                 waning=waning)
 
-    return {'IRS_Halflife': decay, 'IRS_Start': start_days[0], 'IRS_Coverage': coverage}
+    return {"IRS_Halflife": decay, "IRS_Start": start_days[0], "IRS_Coverage": coverage}
+
 
 # act
 def add_healthseeking_by_coverage(cb, coverage=1.0):
     add_health_seeking(cb,
-                       targets=
-                           [{'trigger': 'NewClinicalCase', 'coverage': coverage, 'agemin': 0, 'agemax': 100, 'seek': 1.0,
-                                 'rate': 0.15}],
-                       drug=['Artemether', 'Lumefantrine'],
-                       dosing='FullTreatmentNewDetectionTech',
+                       targets=[{"trigger": "NewClinicalCase",
+                                 "coverage": coverage,
+                                 "agemin": 0,
+                                 "agemax": 100,
+                                 "seek": 1.0,
+                                 "rate": 0.15}],
+                       drug=["Artemether", "Lumefantrine"],
+                       dosing="FullTreatmentNewDetectionTech",
                        nodes={"class": "NodeSetAll"},
                        repetitions=1,
                        tsteps_btwn_repetitions=365,
-                       broadcast_event_name='Received_Treatment')
+                       broadcast_event_name="Received_Treatment")
 
-    return {'ACT_Coverage': coverage}
+    return {"ACT_Coverage": coverage}
