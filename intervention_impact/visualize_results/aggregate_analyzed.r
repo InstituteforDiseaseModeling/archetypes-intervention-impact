6951e90556d68315b765ff6127a2051140c5d6b1
Amelia Bertozzi-Villa <bertozzivill@gmail.com>
2018-10-02 13:56:11 -0700

    update shiny app

--- intervention_impact/visualize_results/aggregate_analyzed.r
+++ intervention_impact/visualize_results/aggregate_analyzed.r
@@ -1,11 +1,10 @@
 library(data.table)
-library(ggplot2)
 
 main_dir <- file.path(Sys.getenv("USERPROFILE"), 
                       "Dropbox (IDM)/Malaria Team Folder/projects/map_intervention_impact/lookup_tables/interactions")
 
-initial <- fread(file.path(main_dir, "../initial/initial_burnin_4.csv"))
-all_data <- fread(file.path(main_dir, "gates_examples", "gates_reruns_bugfix.csv"))
+initial <- fread(file.path(main_dir, "../initial/MAP_II_New_Setup_Burnin.csv"))
+all_data <- fread(file.path(main_dir, "MAP_II_New_Setup.csv"))
 
 # files <- list.files(file.path(main_dir, "version_2"), full.names = T)
 # all_data <- lapply(files, fread)
@@ -19,6 +18,8 @@ for (int in c("ITN", "IRS", "ACT")){
   all_data[get(varname)!=0, Intervention:= paste0(Intervention, int, " ", get(varname), "; ") ]
 }
 
+all_data[, Intervention:= gsub(" $", "", Intervention)]
+
 all_data[Intervention=="", Intervention:="None"]
 all_data <- merge(all_data, initial, by=c("Site_Name", "Run_Number", "x_Temporary_Larval_Habitat"), all=T)
 
@@ -26,5 +27,5 @@ all_data[, Run_Number:=factor(Run_Number)]
 all_data[, mean_initial:= mean(initial_prev), by=list(Site_Name, x_Temporary_Larval_Habitat, Intervention)]
 all_data[, mean_final:=mean(final_prev), by=list(Site_Name, x_Temporary_Larval_Habitat, Intervention)]
 
-write.csv(all_data, file=file.path(main_dir, "gates_examples", "gates_bugfix_lookup.csv"), row.names = F)
+write.csv(all_data, file=file.path(main_dir, "lookup_full_interactions_v3.csv"), row.names = F)
 
