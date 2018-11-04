dd2bb1f14680cd87e2b41eac7c622c543e1079e8
Amelia Bertozzi-Villa <bertozzivill@gmail.com>
2018-11-04 11:52:54 -0800

    beautify new int curves

--- intervention_impact/visualize_results/aggregate_analyzed.r
+++ intervention_impact/visualize_results/aggregate_analyzed.r
@@ -55,14 +55,19 @@ all_data[, Run_Number:=factor(Run_Number)]
 all_data[, mean_initial:= mean(initial_prev), by=list(Site_Name, x_Temporary_Larval_Habitat, Intervention, Coverage)]
 all_data[, mean_final:=mean(final_prev), by=list(Site_Name, x_Temporary_Larval_Habitat, Intervention, Coverage)]
 
-to_plot <- all_data[Site_Name=="aba" & Intervention %like% "mo"]
+to_plot <- all_data[Site_Name=="aba"]
 
+pdf(file.path(Sys.getenv("USERPROFILE"), 
+              "Dropbox (IDM)/Malaria Team Folder/projects/map_intervention_impact/writing_and_presentations/ii_paper",
+              "figures/new_ints.pdf"), height=6, width=7)
 ggplot(to_plot, aes(x=mean_initial, y=mean_final, color=factor(Coverage))) +
   geom_line(size=1.5) +
   geom_abline() + 
   scale_color_manual(values=c("#E9806C", "#F1B657","#B1D066")) +
   facet_wrap( ~ Intervention) +
-  theme_minimal()
+  theme_minimal() +
+  theme(legend.position="none")
+graphics.off()
 
 write.csv(all_data, file=file.path(main_dir, "lookup_full_interactions_experimental.csv"), row.names = F)
 
