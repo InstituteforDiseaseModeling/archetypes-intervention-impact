edf1e3a6f33262934df6e76095b310919213e0b4
Amelia Bertozzi-Villa <bertozzivill@gmail.com>
2019-02-22 18:25:35 +0000

    update file paths to cubes_5km on bucket, initial test runs

--- itn_cube/generate_results/amelia_refactor/acc_deviation_refactored.r
+++ itn_cube/generate_results/amelia_refactor/acc_deviation_refactored.r
@@ -23,6 +23,7 @@
 ##'/home/drive/cubes/5km/worldclim/prec57a0.tif', # 
 # '/home/drive/cubes/5km/Poverty/viirs_nighttime_5km.mean.tif')
 
+# dsub --provider google-v2 --project my-test-project-210811 --image gcr.io/my-test-project-210811/map_geospatial --regions europe-west1 --machine-type n1-standard-64 --logging gs://map_data_z/users/amelia/logs --input-recursive input_dir=gs://map_data_z/cubes_5km joint_dir=gs://map_data_z/users/amelia/itn_cube/joint_data func_dir=gs://map_data_z/users/amelia/itn_cube/code --input database_fname=gs://map_data_z/users/amelia/itn_cube/create_database/output/ITN_final_clean_access_9Feb2019.csv CODE=gs://map_data_z/users/amelia/itn_cube/code/acc_deviation_refactored.r --output-recursive output_dir=gs://map_data_z/users/amelia/itn_cube/access_deviation --command 'Rscript ${CODE}'
 
 
 
@@ -44,11 +45,11 @@ if(Sys.getenv("input_dir")=="") {
   output_dir <- "/Volumes/GoogleDrive/My Drive/itn_cube/access_deviation"
   func_dir <- "/Users/bertozzivill/repos/malaria-atlas-project/itn_cube/generate_results/"
 } else {
-  joint_dir <- Sys.getenv("joint_dir")
-  database_fname <- Sys.getenv("database_fname")
-  input_dir <- Sys.getenv("input_dir")
-  output_dir <- Sys.getenv("output_dir")
-  func_dir <- Sys.getenv("func_dir")
+  joint_dir <- Sys.getenv("joint_dir") # location for shared datasets across itn cube scripts
+  database_fname <- Sys.getenv("database_fname") # location of output file from generate_database_refactored.r
+  input_dir <- Sys.getenv("input_dir") # here, location of covariate data 
+  output_dir <- Sys.getenv("output_dir") 
+  func_dir <- Sys.getenv("func_dir") # code directory for function scripts
 }
 
 # load relevant functions
@@ -56,7 +57,7 @@ source(file.path(func_dir, "acc_deviation_functions.r"))
 source(file.path(func_dir, "algorithm.V1.R"))
 source(file.path(func_dir, "INLAFunctions.R"))
 
-output_fname <- file.path(output_dir, 'ITN_cube_access_dynamic_access deviation_11Feb2019.Rdata')
+output_fname <- file.path(output_dir, 'ITN_cube_access_dynamic_access deviation_22Feb2019.Rdata')
 
 # Load data from create_database.r  ------------------------------------------------------------
 data<-read.csv(database_fname)
@@ -65,19 +66,24 @@ data<-data[!is.na(data$year),]
 ### Load covariates  ----------------------------------------------------------------------------#######################  
 
 # Load dynamic covariates  ------------------------------------------------------------
-foldersd<-c('/home/drive/cubes/5km/LST_day/mean/',
-            #'/home/drive/cubes/5km/LST_delta/mean/',
-            '/home/drive/cubes/5km/LST_night/mean/',
-            '/home/drive/cubes/5km/EVI/mean/',
-            #'/home/drive/cubes/5km/TCB/mean/',
-            '/home/drive/cubes/5km/TCW/mean/',
-            '/home/drive/cubes/5km/TSI/mean/')
+
+dynamic_covnames <- c("LST_day_mean", "LST_night_mean", "EVI_mean", "TCW_mean", "TSI_mean")
+
+
+
+#' foldersd<-c('/home/drive/cubes/5km/LST_day/mean/',
+#'             #'/home/drive/cubes/5km/LST_delta/mean/',
+#'             '/home/drive/cubes/5km/LST_night/mean/',
+#'             '/home/drive/cubes/5km/EVI/mean/',
+#'             #'/home/drive/cubes/5km/TCB/mean/',
+#'             '/home/drive/cubes/5km/TCW/mean/',
+#'             '/home/drive/cubes/5km/TSI/mean/')
 
 # find raster cell ids from access/use data 
 dates<-as.Date(as.yearmon(data$year))
 split.dates<-str_split_fixed(dates, "-", 3)
 uniques<-unique(split.dates)
-l=length(foldersd)
+l=length(dynamic_covnames)
 cell<-data$cellnumber
 
 # extract covariates for unique year-months		
@@ -88,11 +94,12 @@ covs.list.dyn<-foreach(i=1:nrow(uniques)) %dopar% { # loop through unique names
   tmp<-matrix(NA,nrow=length(un.cells),ncol=l)
   for(j in 1:l){
     year<-as.numeric(uniques[i,1])
+    cov_dir <- file.path(input_dir, dynamic_covnames[j])
     
-    while(!file.exists(paste0(foldersd[j],year,'.',uniques[i,2],'.mean.tif'))){	# some years have no data yet
+    while(!file.exists(paste0(cov_dir, "/", year,'.',uniques[i,2],'.mean.tif'))){	# some years have no data yet
       year=year-1
     }
-    r=raster(paste0(foldersd[j],year,'.',uniques[i,2],'.mean.tif'))
+    r=raster(paste0(cov_dir, "/", year,'.',uniques[i,2],'.mean.tif'))
     NAvalue(r)=-9999
     tmp[,j]<-r[un.cells]
   }
@@ -100,7 +107,7 @@ covs.list.dyn<-foreach(i=1:nrow(uniques)) %dopar% { # loop through unique names
 }
 
 # assign these to the correct cells in data
-dynamic.covs<-matrix(NA,nrow=nrow(data),ncol=length(foldersd))
+dynamic.covs<-matrix(NA,nrow=nrow(data),ncol=length(dynamic_covnames))
 for(i in 1:nrow(uniques)){
   wh<-split.dates[,1]==uniques[i,1] & split.dates[,2]==uniques[i,2]
   dynamic.covs[wh,]<-covs.list.dyn[[i]]
@@ -109,14 +116,15 @@ for(i in 1:nrow(uniques)){
 colnames(dynamic.covs)<-c('lst_day','lst_night','evy','tcw','tsi')
 
 # Load year-only (??) covariates  ------------------------------------------------------------
-foldery<-c('/home/drive/cubes/5km/IGBP_Landcover/Fraction/','/home/drive/cubes/5km/AfriPop/')
+# foldery<-c('/home/drive/cubes/5km/IGBP_Landcover/Fraction/','/home/drive/cubes/5km/AfriPop/')
+year_covnames <- c("IGBP_Landcover_Fraction", "AfriPop")
 
 uniques<-unique(split.dates[,1])
 
 l=17+1 # 17 fraction classes and 1 afripop
 cell<-data$cellnumber
 
-#compute covariates for unique year-months	 (surely just years? --abv)	
+#compute covariates for unique year-months
 registerDoParallel(62)
 covs.list.year<-foreach(i=1:length(uniques)) %dopar% { # loop through unique names
   wh<-split.dates[,1]==uniques[i] 
@@ -125,17 +133,17 @@ covs.list.year<-foreach(i=1:length(uniques)) %dopar% { # loop through unique nam
   for(j in 1:(l-1)){
     # no land cover for 2013
     if(uniques[i]>2012){
-      r=raster(paste0(foldery[1],'2012','.fraction.class.',j-1,'.tif'))
+      r=raster(paste0(file.path(input_dir, "IGBP_Landcover_Fraction"), "/",'2012','.fraction.class.',j-1,'.tif'))
     }else{
-      r=raster(paste0(foldery[1],uniques[i],'.fraction.class.',j-1,'.tif'))
+      r=raster(paste0(file.path(input_dir, "IGBP_Landcover_Fraction"), "/",uniques[i],'.fraction.class.',j-1,'.tif'))
     }		
     NAvalue(r)=-9999
     tmp[,j]<-r[un.cells]
   }
   if(uniques[i]>2015){
-    r=raster(paste0(foldery[2],'2015','.total.population.tif'))
+    r=raster(paste0(file.path(input_dir, "AfriPop"), "/",'2015','.total.population.tif'))
   }else{
-    r=raster(paste0(foldery[2],uniques[i],'.total.population.tif'))
+    r=raster(paste0(file.path(input_dir, "AfriPop"), "/",uniques[i],'.total.population.tif'))
   }	
   NAvalue(r)=-9999
   tmp[,l]<-r[un.cells]
@@ -155,20 +163,31 @@ yearonly.covs<-yearonly.covs[,-14] # remove landcover 13  - Urban and built-up  
 
 # Load static covariates  ------------------------------------------------------------
 
-folderss<-c('/home/drive/cubes/5km/Topographic/Africa_TMI_90m.mean.tif',
-            '/home/drive/cubes/5km/Topographic/Africa_SRTM_90m.mean.tif',
-            '/home/drive/cubes/5km/Topographic/Africa_slope_90m.mean.tif',
-            #'/home/drive/cubes/5km/Topographic/Africa_FA_90m.mean.tif',
-            '/home/drive/cubes/5km/Seasonality/pf_seasonality.tif',
-            '/home/drive/cubes/5km/Poverty/PET_1950-2000_5km.mean.tif',
-            '/home/drive/cubes/5km/Poverty/AI_1950-2000_5km.mean.tif',
-            '/home/drive/cubes/5km/Poverty/accessibility_50k_5km.mean.tif',
-            #'/home/drive/cubes/5km/worldclim/prec57a0.tif',
-            '/home/drive/cubes/5km/Poverty/viirs_nighttime_5km.mean.tif')
-#'/home/drive/cubes/5km/Poverty/DMSP_F18_5km.mean.tif')
+static_fnames <- c("Topographic/Africa_TMI_90m.mean.tif",
+                   "Topographic/Africa_SRTM_90m.mean.tif",
+                   "Topographic/Africa_slope_90m.mean.tif",
+                   "Seasonality/pf_seasonality.tif",
+                   "Poverty/PET_1950-2000_5km.mean.tif",
+                   "Poverty/AI_1950-2000_5km.mean.tif",
+                   "Poverty/accessibility_50k_5km.mean.tif",
+                   "Poverty/viirs_nighttime_5km.mean.tif")
+
+static_covnames <- file.path(input_dir, static_fnames)
+
+#' folderss<-c('/home/drive/cubes/5km/Topographic/Africa_TMI_90m.mean.tif',
+#'             '/home/drive/cubes/5km/Topographic/Africa_SRTM_90m.mean.tif',
+#'             '/home/drive/cubes/5km/Topographic/Africa_slope_90m.mean.tif',
+#'             #'/home/drive/cubes/5km/Topographic/Africa_FA_90m.mean.tif',
+#'             '/home/drive/cubes/5km/Seasonality/pf_seasonality.tif',
+#'             '/home/drive/cubes/5km/Poverty/PET_1950-2000_5km.mean.tif',
+#'             '/home/drive/cubes/5km/Poverty/AI_1950-2000_5km.mean.tif',
+#'             '/home/drive/cubes/5km/Poverty/accessibility_50k_5km.mean.tif',
+#'             #'/home/drive/cubes/5km/worldclim/prec57a0.tif',
+#'             '/home/drive/cubes/5km/Poverty/viirs_nighttime_5km.mean.tif')
+#' #'/home/drive/cubes/5km/Poverty/DMSP_F18_5km.mean.tif')
 
 
-st<-stack(folderss)
+st<-stack(static_covnames)
 NAvalue(st)=-9999
 
 static.covs<-st[data$cellnumber]
@@ -226,10 +245,8 @@ if(STANDARD){
 
 drow<-nrow(data) ## Number of data points
 # load country rasters
-# todo: move both of these datasets into a shared folder across all scripts
 cn<-raster(file.path(joint_dir, 'african_cn5km_2013_no_disputes.tif'))
 NAvalue(cn)<--9999
-# todo: transfer from '/home/backup/ITNcube/country_table_populations.csv'
 POPULATIONS<-read.csv(file.path(joint_dir, 'country_table_populations.csv')) # load table to match gaul codes to country names
 
 # adjust timing of data? this feels important****
